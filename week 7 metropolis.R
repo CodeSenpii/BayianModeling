bde54c81e88925183c2e95fd3723eb3ecf2d3936
tonnykwon <tonnykwon@naver.com>
2018-10-20 11:59:01 +0900

    metropolis

--- week 7 metropolis.R
+++ week 7 metropolis.R
@@ -1,1 +1,23 @@
 # week7 Metropolis example
+n = 271
+ybar = 1.4
+s.2= 1.684
+
+mu0 = 1.10
+tau.2.0 = sigma.2.0 = 1.17
+nu0 = 1
+
+dinvchisq = function(x, df, scale){
+  if(x>0)
+    ((df/2)^(df/2) / gamma(df/2)) * scale&df * x^(-(df/2+1))*
+    exp(-df*scale/(2*x))
+  else 0
+}
+
+likelihood = function(mu, sigma.2){
+  sigma.2^(-n)
+}
+
+
+
+
